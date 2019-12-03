50dcf9afd321f76081e33f2d0fd6c3283e028856
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-03 02:30:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,6 +37,6 @@ end
 # look through array of strings to find and return the first string
 # that is a type of cheese
 def find_the_cheese(array)
-  # the array below is here to help
+  array.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
