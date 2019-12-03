c1d5af400975f4d097cd44797a48542748ca2104
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-03 02:58:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -42,7 +42,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   array.detect do |cheese|
-    if cheese_types.include?("cheese")
+    if cheese_types.include?(cheese)
       # return the first string that is a type of cheese
       return cheese
     else
