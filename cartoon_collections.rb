9656bcbb1ad87713c2bd0ea806f9475accdbfa26
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-03 02:43:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -45,7 +45,7 @@ def find_the_cheese(array)
     if cheese_types.include?(cheese)
       # return the first string that is a type of cheese
       return cheese
-    else  
+    else  return nil
     end
   end
 end
